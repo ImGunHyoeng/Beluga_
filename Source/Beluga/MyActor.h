e274e6cc5bbdeb3aff678c730fe9e71d8b3ad5e1
ImGunHyoeng <ynoe30@gmail.com>
2023-07-17 18:13:30 +0900

    learn reflection

---
+++ Source/Beluga/MyActor.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class BELUGA_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	// 이 actor의 속성에 대한 기본값을 설정합니다.
+	AMyActor();
+
+
+protected:
+	// 게임이 시작되거나 생성될 때 호출됩니다.
+	virtual void BeginPlay() override;
+
+public:
+	// 매 프레임마다 호출됨
+	virtual void Tick(float DeltaTime) override;
+};
